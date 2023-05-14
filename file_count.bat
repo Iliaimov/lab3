b0acfb11a1e210905b79742354fb2e3ce8be32e2
Illia Rybenok <130609703+Iliaimov@users.noreply.github.com>
2023-05-14 22:36:59 +0300

    file_count.bat

--- file_count.bat
+++ file_count.bat
@@ -6,7 +6,6 @@ set "file_count=0"
 for /r "%~dp0%subdirectory%" %%A in (*) do set /a "file_count+=1"
 echo Кількість файлів в підкаталозі "%subdirectory%": %file_count%
 
-REM Підрахунок кількості файлів в підкаталозі "batch"
 set "subdirectory=batch"
 set "file_count=0"
 for /f %%B in ('dir /a-d-h /b /s "%~dp0%subdirectory%" ^| find /v /c "::"') do set "file_count=%%B"
